0820471da591e843752f7ebb5b72bebdfc75bbb1
ali7haider <ali7haider.dev@gmail.com>
2024-07-21 01:42:25 +0500

    Array #1: Two Sum

---
+++ Arrays/1. Two Sum.py
@@ -0,0 +1,36 @@
+"""Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]"""
+
+"""N^2 Solution"""
+def nSquareSolution(nums,target):
+    for i in range(len(nums)):
+        for j in range(i+1,len(nums)):
+            if nums[i]+nums[j]==target:
+                return [i,j]
+            
+
+"""N Soluion"""
+def nSolution(nums,target):
+    mapValue={}
+    for i in range(len(nums)):
+        if target-nums[i] in mapValue:
+            return [mapValue[target-nums[i]],i]
+        mapValue[nums[i]]=i
+nums = [2,7,11,15]
+target = 13
+print(nSquareSolution(nums,target))
